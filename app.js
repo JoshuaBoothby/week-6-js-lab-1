c6e82dc4e66fcbfcbc80206f0fb80e0d68e65360
Joshua Boothby <Mona.Lisa@example.com>
2025-01-09 14:47:13 -0600

    added nav bar and started blog posts

--- app.js
+++ app.js
@@ -3,22 +3,24 @@
 // Number Array Manipulation
 let numbers = [100, 456, 98, 45, 43, 73];
 
-
 let allNumbers = document.getElementById("allNumbers");
 let evenNumbers = document.getElementById("evenNumbers");
 let oddNumbers = document.getElementById("oddNumbers");
 
+// All numbers
 for (let i = 0; i < numbers.length; i++) {
   allNumbers.innerHTML += `<li>${numbers[i]}</li>`;
   console.log(numbers[i]);
 }
 
+// Even numbers
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] % 2 === 0) {
     evenNumbers.innerHTML += `<li>${numbers[i]}</li>`;
   }
 }
 
+// Odd numbers
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] % 2 !== 0) {
     oddNumbers.innerHTML += `<li>${numbers[i]}</li>`;
@@ -27,18 +29,33 @@ for (let i = 0; i < numbers.length; i++) {
 
 // Object Array and Bootstrap Rendering
 let blogPosts = [
-  { id: 1, title: "Blog Post 1", author: "Author A", date: "2023-11-15" },
-  { id: 2, title: "Blog Post 2", author: "Author B", date: "2023-12-01" },
+  {
+    id: 1,
+    title: "Blog Post 1",
+    author: "Author A",
+    date: "2023-11-15",
+    src: "https://via.placehold.com/100x100",
+  },
+  {
+    id: 2,
+    title: "Blog Post 2",
+    author: "Author B",
+    date: "2023-12-01",
+    src: "https://via.placehold.com/100x100",
+  },
 ];
 
-console.log(blogPosts.length);
+let blogPostsContainer = document.getElementById("blogPostsContainer");
+
+// Both blog posts as a Bootstrap card
 for (let i = 0; i < blogPosts.length; i++) {
   let bootstrapCard = `<div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="${blogPosts[i].src}" class="card-img-top" alt="${blogPosts[i].title}">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <h5 class="card-title">${blogPosts[i].title}</h5>
+    <p class="card-text">${blogPosts[i].author}</p>
+    <p class="card-text">${blogPosts[i].date}</p>
   </div>
 </div>`;
+  blogPostsContainer.innerHTML += bootstrapCard;
 }
